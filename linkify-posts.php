d2decc25867f5b059718325f09bfcf3c3a8c6270
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-28 15:10:11 +0000

    * Update to v2.0 (first release in plugin repository) (includes changes from unreleased v1.5) * Rename plugin from 'Linkify Post IDs' to 'Linkify Posts' * Rename `linkify_post_ids()` to `c2c_linkify_posts()` (but maintain a deprecated version for backwards compatibility) * Rename 'linkify_post_ids' filter to 'c2c_linkify_posts' (no backwards compatibility support since v1.5 was never released) * Allow linkifying posts specified by slug (in addition to existing support for posts specified by ID) * Change description * Add filter 'linkify_post_ids' to respond to the function of the same name so that users can use the do_action() notation for invoking template tags * Fix to prevent PHP notice * Wrap function in if(!function_exists()) check * Reverse order of implode() arguments * Remove docs from top of plugin file (all that and more are in readme.txt) * Add package info to top of plugin file * Add PHPDoc documentation * Note compatibility with WP 2.8+, 2.9+, 3.0+ * Drop support for version of WP older than 2.8 * Minor tweaks to code formatting (spacing) * Add Changelog, Filters, and Upgrade Notice sections to readme.txt * Update copyright date * Remove trailing whitespace
    git-svn-id: https://plugins.svn.wordpress.org/linkify-posts/tags/2.0@281918 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ linkify-posts.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * @package Linkify_Posts
+ * @author Scott Reilly
+ * @version 2.0
+ */
+/*
+Plugin Name: Linkify Posts
+Version: 2.0
+Plugin URI: http://coffee2code.com/wp-plugins/linkify-posts/
+Author: Scott Reilly
+Author URI: http://coffee2code.com
+Description: Turn a string, list, or array of post IDs and/or slugs into a list of links to those posts.
+
+Compatible with WordPress 2.8+, 2.9+, 3.0+.
+
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/linkify-posts/
+
+*/
+
+/*
+Copyright (c) 2007-2010 by Scott Reilly (aka coffee2code)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+if ( !function_exists( 'c2c_linkify_posts' ) ) :
+/**
+ * Displays links to each of any number of posts specified via post IDs and/or slugs
+ *
+ * @since 2.0
+ *
+ * @param int|array $posts A single post ID/slug, or multiple post IDs/slugs defined via an array, or multiple posts IDs/slugs defined via a comma-separated and/or space-separated string
+ * @param string $before (optional) To appear before the entire post listing (if posts exist or if 'none' setting is specified)
+ * @param string $after (optional) To appear after the entire post listing (if posts exist or if 'none' setting is specified)
+ * @param string $between (optional) To appear between all posts
+ * @param string $before_last (optional) To appear between the second-to-last and last element, if not specified, value of $between is used
+ * @param string $none (optional) To appear when no posts have been found.  If blank, then the entire function doesn't display anything
+ */
+function c2c_linkify_posts( $posts, $before = '', $after = '', $between = ', ', $before_last = '', $none = '' ) {
+	if ( empty( $posts ) )
+		$posts = array();
+	elseif ( !is_array( $posts ) )
+		$posts = explode( ',', str_replace( array( ', ', ' ', ',' ), ',', $posts ) );
+
+	if ( empty( $posts ) )
+		$response = '';
+	else {
+		$links = array();
+		foreach ( $posts as $id ) {
+			if ( 0 == (int) $id ) {
+				$my_q = new WP_Query( array( 'name' => $id ) );
+				if ( $my_q->have_posts() )
+					$id = $my_q->posts[0]->ID;
+			}
+			if ( 0 == (int) $id )
+				continue;
+			$title = get_the_title( $id );
+			if ( $title )
+				$links[] = sprintf(
+					'<a href="%1$s" title="%2$s">%3$s</a>',
+					get_permalink( $id ),
+					esc_attr( sprintf( __( "View post: %s" ), $title ) ),
+					$title
+				);
+		}
+		if ( empty( $before_last ) )
+			$response = implode( $between, $links );
+		else {
+			switch ( $size = sizeof( $links ) ) {
+				case 1:
+					$response = $links[0];
+					break;
+				case 2:
+					$response = $links[0] . $before_last . $links[1];
+					break;
+				default:
+					$response = implode( $between, array_slice( $links, 0, $size-1 ) ) . $before_last . $links[$size-1];
+			}
+		}
+	}
+	if ( empty( $response ) ) {
+		if ( empty( $none ) )
+			return;
+		$response = $none;
+	}
+	echo $before . $response . $after;
+}
+add_action( 'c2c_linkify_posts', 'c2c_linkify_posts', 10, 6 );
+endif;
+
+if ( !function_exists( 'linkify_post_ids' ) ) :
+/**
+ * Displays links to each of any number of posts specified via post IDs and/or slugs
+ *
+ * @since 1.0
+ * @deprecated 2.0 Use c2c_linkify_posts() instead
+ */
+function linkify_post_ids( $posts, $before = '', $after = '', $between = ', ', $before_last = '', $none = '' ) {
+	_deprecated_function( 'linkify_post_ids', '2.0', 'c2c_linkify_posts' );
+	return c2c_linkify_posts( $posts, $before, $after, $between, $before_last, $none );
+}
+endif;
+?>
