d69dde9ddc27173ab7ca648d19451eec0377af6d
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-10 18:48:16 +0000

    * Update to v2.1.4 * Add check to prevent execution of code if file is directly accessed * Note compatibility through WP 3.5+ * Update copyright date (2013) * Create repo's WP.org assets directory * Move screenshot into repo's assets directory
    git-svn-id: https://plugins.svn.wordpress.org/linkify-posts/tags/2.1.4@636696 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- linkify-posts.php
+++ linkify-posts.php
@@ -2,11 +2,11 @@
 /**
  * @package Linkify_Posts
  * @author Scott Reilly
- * @version 2.1.3
+ * @version 2.1.4
  */
 /*
 Plugin Name: Linkify Posts
-Version: 2.1.3
+Version: 2.1.4
 Plugin URI: http://coffee2code.com/wp-plugins/linkify-posts/
 Author: Scott Reilly
 Author URI: http://coffee2code.com/
@@ -14,7 +14,7 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Description: Turn a string, list, or array of post IDs and/or slugs into a list of links to those posts.
 
-Compatible with WordPress 2.8 through 3.4+.
+Compatible with WordPress 2.8 through 3.5+.
 
 =>> Read the accompanying readme.txt file for instructions and documentation.
 =>> Also, visit the plugin's homepage for additional information and updates.
@@ -23,7 +23,7 @@ Compatible with WordPress 2.8 through 3.4+.
 */
 
 /*
-	Copyright (c) 2007-2012 by Scott Reilly (aka coffee2code)
+	Copyright (c) 2007-2013 by Scott Reilly (aka coffee2code)
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
@@ -39,6 +39,8 @@ Compatible with WordPress 2.8 through 3.4+.
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
+defined( 'ABSPATH' ) or die();
 
 require_once( dirname( __FILE__ ) . '/linkify-posts.widget.php' );
 
